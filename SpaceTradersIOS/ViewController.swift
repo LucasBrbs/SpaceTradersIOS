ac27b5d72392c1cd4e1f59a207fd8d8c305616e2
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2022-10-11 09:30:03 -0300

    Initial Commit

---
+++ SpaceTradersIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceTradersIOS
+//
+//  Created by Lucas Barbosa de Oliveira on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
